6bda4ebb87bd160039493104cf05360fac985dab
danielcasadofauli <daniel.casado@students.salle.url.edu>
2018-12-27 23:30:02 +0100

    fix: Assigned new value to minimum initial altitude
    The minimum initial altitude assigned of 0 made it impossible for most altitudes to replace that initial one due to be the lowest altitude possible, not taking in account under sea levels. It was replaced by the indetermiant INFINITY as a constant that can't be ever smaller than other number

--- model/destination/destination.c
+++ model/destination/destination.c
@@ -90,7 +90,7 @@ double travelThroughAltitude(double ** travelAltitude, int i, int j, int n) {
 	}
 
 	int newI = i, newJ = j, min = NORTH;
-	double minAlt = 0;
+	double minAlt = INFINITY;
 
 	for (int k = 0; k < DIRECTIONS; k++) {
 		double alt = checkAltitudeInDirection(travelAltitude, i, j, k, n);
@@ -103,7 +103,7 @@ double travelThroughAltitude(double ** travelAltitude, int i, int j, int n) {
 
 	advanceInDirection(&newI, &newJ, min);
 
-	return travelAltitude[i][j] + travelThroughAltitude(travelAltitude, i, j, n);
+	return travelAltitude[i][j] + travelThroughAltitude(travelAltitude, newI, newJ, n);
 }
 
 // Retorna una estimació del temps de viatge des de BCN segons l'altitud
